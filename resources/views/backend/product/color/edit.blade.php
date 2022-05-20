7695318cc92b30ca1d5b45f01df884b584eae4a7
AvinashSingh <avinash.orrish@gmail.com>
2022-05-20 18:22:28 +0530

    color modules completetd

--- resources/views/backend/product/color/edit.blade.php
+++ resources/views/backend/product/color/edit.blade.php
@@ -2,13 +2,18 @@
 
 @section('content')
 
-<div class="aiz-titlebar text-left mt-2 mb-3">
-    <h5 class="mb-0 h6">{{translate('Color Information')}}</h5>
-</div>
-
-<div class="col-lg-8 mx-auto">
-    <div class="card">
-        <div class="card-body p-0">
+<div class="container-xxl flex-grow-1 container-p-y">
+   <h4 class="fw-bold py-3 mb-4">
+      {{translate('Color Information')}}
+   </h4>
+   <div class="row">
+      <div class="col-md-12">
+         <div class="card">
+            <div class="card-header">
+               <h5 class="mb-0 h6">{{translate('Color Information')}}</h5>
+               <a href="{{ url()->previous() }}" style="border-radius:5px;position: absolute;z-index: 999;right: 30px;top:30px;border: 1px solid #696cff;padding:5px 20px;   background: #696cff;color: #fff;">Back</a>
+            </div>
+            <div class="card-body">
             @if ($errors->any())
                 <div class="alert alert-danger">
                     <ul>
@@ -18,31 +23,36 @@
                     </ul>
                 </div>
             @endif
-            <form class="p-4" action="{{ route('colors.update', $color->id) }}" method="POST">
-                <input name="_method" type="hidden" value="POST">
-                @csrf
-                <div class="form-group row">
-                    <label class="col-sm-3 col-from-label" for="name">
-                        {{ translate('Name')}} 
-                    </label>
-                    <div class="col-sm-9">
+               <form class="p-4 ng-pristine ng-valid" action="{{ route('colors.update', $color->id) }}" method="POST">
+                  <input name="_method" type="hidden" value="POST">
+                  @csrf
+                  <div class="form-group row">
+                     <label class="form-label" for="name">
+                     {{ translate('Name')}}
+                     </label>
+                     <div class="col-sm-91">
                         <input type="text" placeholder="{{ translate('Name')}}" id="name" name="name" class="form-control" required value="{{ $color->name }}">
-                    </div>
-                </div>
-                <div class="form-group row">
-                    <label class="col-sm-3 col-from-label" for="code">
-                        {{ translate('Color Code')}} 
-                    </label>
-                    <div class="col-sm-9">
-                        <input type="text" placeholder="{{ translate('Color Code')}}" id="code" name="code" class="form-control" required value="{{ $color->code }}">
-                    </div>
-                </div>
-                <div class="form-group mb-0 text-right">
-                    <button type="submit" class="btn btn-primary">{{translate('Save')}}</button>
-                </div>
-            </form>
-        </div>
-    </div>
+                     </div>
+                  </div>
+                  <br>		
+                  <div class="form-group row">
+                     <label class="form-label" for="code">
+                     {{ translate('Color Code')}} 
+                     </label>
+                     <div class="col-sm-91">
+                        <input type="text"  placeholder="{{ translate('Color Code')}}" id="code" name="code" class="form-control" required value="{{ $color->code }}">
+                     </div>
+                  </div>
+                  <br>
+                  <div class="form-group mb-0 text-right">
+                     <button type="submit" class="btn btn-primary">{{translate('Save')}}</button>
+                  </div>
+               </form>
+            </div>
+         </div>
+      </div>
+   </div>
 </div>
-
+<div class="content-backdrop fade"></div>
+</div>
 @endsection
--- resources/views/backend/product/color/index.blade.php
+++ resources/views/backend/product/color/index.blade.php
@@ -1,99 +1,98 @@
 @extends('backend.layouts.app')
 
 @section('content')
-
-    <div class="aiz-titlebar text-left mt-2 mb-3">
-        <div class="align-items-center">
-            <h1 class="h3">{{ translate('All Colors') }}</h1>
-        </div>
-    </div>
-
-    <div class="row">
-        <div class="col-md-7">
-            <div class="card">
-                <form class="" id="sort_colors" action="" method="GET">
-                    <div class="card-header">
-                        <h5 class="mb-0 h6">{{ translate('Colors') }}</h5>
-                        <div class="col-md-5">
-                            <div class="form-group mb-0">
-                                <input type="text" class="form-control form-control-sm" id="search" name="search"
-                                    @isset($sort_search) value="{{ $sort_search }}" @endisset
-                                    placeholder="{{ translate('Type color name & Enter') }}">
+    <div class="content-wrapper">
+        <div class="container-xxl flex-grow-1 container-p-y">
+            <h4 class="fw-bold py-3 mb-4">
+            {{ translate('All Colors') }}
+            </h4>
+            <!-- DataTable with Buttons -->
+            <div class="row">
+                <div class="col-md-7">
+                    <div class="card">
+                        <form class="ng-pristine ng-valid" id="sort_colors" action="" method="GET">
+                            <div class="card-header" style="padding-bottom: 0px;">
+                                <div class="row">
+                                    <div class="col-md-7">
+                                        <h5 class="mb-0 h6">{{ translate('Colors') }}</h5>
+                                    </div>
+                                    <div class="col-md-5">
+                                        <div class="form-group mb-0">
+                                            <input type="text" class="form-control form-control-sm" id="search" name="search"
+                                                            @isset($sort_search) value="{{ $sort_search }}" @endisset
+                                                            placeholder="{{ translate('Type color name & Enter') }}">
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
-                        </div>
-                    </div>
-                </form>
-                
-                <div class="card-body">
-                    <table class="table aiz-table mb-0">
-                        <thead>
-                            <tr>
-                                <th>#</th>
-                                <th>{{ translate('Name') }}</th>
-                                <th class="text-right">{{ translate('Options') }}</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @foreach ($colors as $key => $color)
-                                <tr>
-                                    <td>{{ ($key+1) + ($colors->currentPage() - 1)*$colors->perPage() }}</td>
-                                    <td>{{ $color->name }}</td>
-                                    <td class="text-right">
-                                        <a class="btn btn-soft-primary btn-icon btn-circle btn-sm"
-                                            href="{{ route('colors.edit', ['id' => $color->id, 'lang' => env('DEFAULT_LANGUAGE')]) }}"
-                                            title="{{ translate('Edit') }}">
-                                            <i class="las la-edit"></i>
-                                        </a>
-                                        <a href="#" class="btn btn-soft-danger btn-icon btn-circle btn-sm confirm-delete"
-                                            data-href="{{ route('colors.destroy', $color->id) }}"
-                                            title="{{ translate('Delete') }}">
-                                            <i class="las la-trash"></i>
-                                        </a>
-                                    </td>
+                        </form>
+
+                            <div class="card-body">
+                            <table class="table aiz-table mb-0 footable footable-1 breakpoint-lg" style="">
+                            <thead>
+                                <tr class="footable-header">
+                                    <th class="footable-first-visible" style="display: table-cell;">#</th>
+                                    <th style="display: table-cell;">{{ translate('Name') }}</th>
+                                    <th class="text-right footable-last-visible" style="display: table-cell;">{{ translate('Options') }}</th>
                                 </tr>
+                            </thead>
+                            <tbody>
+               @foreach ($colors as $key => $color)
+                  <tr>
+                     <td class="footable-first-visible" style="display: table-cell;">{{ ($key+1) + ($colors->currentPage() - 1)*$colors->perPage() }}</td>
+                     <td style="display: table-cell;">{{ $color->name }}</td>
+                     <td> 
+                                    <a href="{{ route('colors.edit', ['id' => $color->id, 'lang' => env('DEFAULT_LANGUAGE')]) }}"
+                                            title="{{ translate('Edit') }}" class="btn btn-sm btn-icon item-edit"><i class="bx bxs-edit"></i></a>
+                                    <a href="{{ route('colors.destroy', $color->id) }}"
+                                            title="{{ translate('Delete') }}">  <i class="bx bx-trash me-1" aria-hidden="true"></i></a>
+                                 </td>
+                  </tr>
+                  @endforeach
+            
+                 
+               </tbody>
+            </table>
+          
+            </div>
+		      <div class="aiz-pagination">
+              {{ $colors->appends(request()->input())->links() }}
+            </div>
+          </div>
+          </div>
+          <div class="col-md-5">
+           <div class="card">
+           <div class="card-header">
+            <h5 class="mb-0 h6">{{ translate('Add New Color') }}</h5>
+           </div>
+           <div class="card-body">
+                @if ($errors->any())
+                    <div class="alert alert-danger">
+                        <ul>
+                            @foreach ($errors->all() as $error)
+                                <li>{{ $error }}</li>
                             @endforeach
-                        </tbody>
-                    </table>
-                    <div class="aiz-pagination">
-                        {{ $colors->appends(request()->input())->links() }}
+                        </ul>
                     </div>
-                </div>
-            </div>
-        </div>
-        <div class="col-md-5">
-            <div class="card">
-                <div class="card-header">
-                    <h5 class="mb-0 h6">{{ translate('Add New Color') }}</h5>
-                </div>
-                <div class="card-body">
-                    @if ($errors->any())
-                        <div class="alert alert-danger">
-                            <ul>
-                                @foreach ($errors->all() as $error)
-                                    <li>{{ $error }}</li>
-                                @endforeach
-                            </ul>
-                        </div>
-                    @endif
-                    <form action="{{ route('colors.store') }}" method="POST">
-                        @csrf
-                        <div class="form-group mb-3">
-                            <label for="name">{{ translate('Name') }}</label>
-                            <input type="text" placeholder="{{ translate('Name') }}" id="name" name="name"
+                @endif
+            <form id="sort_colors" action="{{ route('colors.store') }}" method="POST">
+            @csrf
+               <div class="form-group mb-3">
+                  <label for="name">{{ translate('Name') }}</label>
+                  <input type="text"placeholder="{{ translate('Name') }}" id="name" name="name"
                                 class="form-control" value="{{ old('name') }}" required>
-                        </div>
-                        <div class="form-group mb-3">
-                            <label for="name">{{ translate('Color Code') }}</label>
-                            <input type="text" placeholder="{{ translate('Color Code') }}" id="code" name="code"
+               </div>
+               <div class="form-group mb-3">
+                  <label for="name">{{ translate('Color Code') }}</label>
+                  <input type="text" placeholder="{{ translate('Color Code') }}" id="code" name="code"
                                 class="form-control" value="{{ old('code') }}" required>
-                        </div>
-                        <div class="form-group mb-3 text-right">
-                            <button type="submit" class="btn btn-primary">{{ translate('Save') }}</button>
-                        </div>
-                    </form>
-                </div>
-            </div>
-            <div class="card">
+               </div>
+               <div class="form-group mb-3 text-right">
+                  <button type="submit" class="btn btn-primary">{{ translate('Save') }}</button>
+               </div>
+            </form>
+         </div>
+         <div class="card">
                 <div class="card-header">
                     <h3 class="mb-0 h6">{{translate('Color filter activation')}}</h3>
                 </div>
@@ -104,10 +103,7 @@
                     </label>
                 </div>
             </div>
-        </div>
-    </div>
-
-
+    
 @endsection
 
 @section('modal')
