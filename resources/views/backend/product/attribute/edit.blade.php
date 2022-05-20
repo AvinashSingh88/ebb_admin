8de931dd277ebc86a872d26ac275afb845a9a1e4
AvinashSingh <avinash.orrish@gmail.com>
2022-05-20 16:25:27 +0530

    attribute module completed

--- resources/views/backend/product/attribute/edit.blade.php
+++ resources/views/backend/product/attribute/edit.blade.php
@@ -1,40 +1,65 @@
 @extends('backend.layouts.app')
+@section('styles')
+<style>
+  table.dataTable thead .sorting:before,
+  table.dataTable thead .sorting:after {
+    display: none;
+  }
 
+  tfoot img,
+  tr img,
+  td img,
+  th img {
+    width: 50px;
+  }
+</style>
+@endsection
 @section('content')
 
-<div class="aiz-titlebar text-left mt-2 mb-3">
-    <h5 class="mb-0 h6">{{translate('Attribute Information')}}</h5>
-</div>
+<div class="container-xxl flex-grow-1 container-p-y">
+  <h4 class="fw-bold py-3 mb-4">
+    {{translate('Attribute Information')}}
+  </h4>
 
-<div class="col-lg-8 mx-auto">
-    <div class="card">
-        <div class="card-body p-0">
+
+  <div class="row">
+
+    <div class="col-md-12">
+      <div class="card">
+        <div class="card-header">
           <ul class="nav nav-tabs nav-fill border-light">
             @foreach (\App\Models\Language::all() as $key => $language)
-              <li class="nav-item">
-                <a class="nav-link text-reset @if ($language->code == $lang) active @else bg-soft-dark border-light border-left-0 @endif py-3" href="{{ route('attributes.edit', ['id'=>$attribute->id, 'lang'=> $language->code] ) }}">
-                  <img src="{{ static_asset('assets/img/flags/'.$language->code.'.png') }}" height="11" class="mr-1">
-                  <span>{{ $language->name }}</span>
-                </a>
-              </li>
-             @endforeach
+            <li class="nav-item">
+              <a class="nav-link text-reset @if ($language->code == $lang) active @else bg-soft-dark border-light border-left-0 @endif py-3" href="{{ route('attributes.edit', ['id'=>$attribute->id, 'lang'=> $language->code] ) }}">
+                <img src="{{ static_asset('assets/img/flags/'.$language->code.'.png') }}" height="11" class="mr-1">
+                <span>{{ $language->name }}</span>
+              </a>
+            </li>
+            @endforeach
           </ul>
-          <form class="p-4" action="{{ route('attributes.update', $attribute->id) }}" method="POST">
-              <input name="_method" type="hidden" value="PATCH">
-              <input type="hidden" name="lang" value="{{ $lang }}">
-              @csrf
-              <div class="form-group row">
-                  <label class="col-sm-3 col-from-label" for="name">{{ translate('Name')}} <i class="las la-language text-danger" title="{{translate('Translatable')}}"></i></label>
-                  <div class="col-sm-9">
-                      <input type="text" placeholder="{{ translate('Name')}}" id="name" name="name" class="form-control" required value="{{ $attribute->getTranslation('name', $lang) }}">
-                  </div>
-              </div>
-              <div class="form-group mb-0 text-right">
-                  <button type="submit" class="btn btn-primary">{{translate('Save')}}</button>
-              </div>
-            </form>
+          <a href="{{ url()->previous() }}" style="border-radius:5px;position: absolute;z-index: 999;right: 30px;top:30px;border: 1px solid #696cff;padding:5px 20px;   background: #696cff;color: #fff;">Back</a>
+
         </div>
+        <div class="card-body">
+          <form class="p-4 ng-pristine ng-valid" action="{{ route('attributes.update', $attribute->id) }}" method="POST">
+            <input name="_method" type="hidden" value="PATCH">
+            <input type="hidden" name="lang" value="{{ $lang }}">
+            @csrf
+            <div class="form-group row">
+              <label class="col-sm-3 col-from-label" for="name">{{ translate('Name')}} <i class="las la-language text-danger" title="{{translate('Translatable')}}"></i></label>
+              <div class="col-sm-91">
+                <input type="text" placeholder="{{ translate('Name')}}" id="name" name="name" class="form-control" required="" value="{{ $attribute->getTranslation('name', $lang) }}">
+              </div>
+            </div><br>
+            <div class="form-group mb-0 text-right">
+              <button type="submit" class="btn btn-primary">Save</button>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
+  </div>
+
 </div>
 
 @endsection
--- resources/views/backend/product/attribute/index.blade.php
+++ resources/views/backend/product/attribute/index.blade.php
@@ -1,40 +1,53 @@
 @extends('backend.layouts.app')
+@section('styles')
+<style>
+	table.dataTable thead .sorting:before,
+	table.dataTable thead .sorting:after {
+		display: none;
+	}
 
+	tfoot img,
+	tr img,
+	td img,
+	th img {
+		width: 50px;
+	}
+</style>
+@endsection
 @section('content')
 
-<div class="aiz-titlebar text-left mt-2 mb-3">
-	<div class="align-items-center">
-		<h1 class="h3">{{translate('All Attributes')}}</h1>
-	</div>
-</div>
 
-<div class="row">
-	<div class="col-md-7">
-		<div class="card">
-			<div class="card-header">
-				<h5 class="mb-0 h6">{{ translate('Attributes')}}</h5>
-			</div>
-			<div class="card-body">
-				<table class="table aiz-table mb-0">
-					<thead>
-						<tr>
-							<th>#</th>
-							<th>{{ translate('Name')}}</th>
-							<th>{{ translate('Values')}}</th>
-							<th class="text-right">{{ translate('Options')}}</th>
-						</tr>
-					</thead>
-					<tbody>
-						@foreach($attributes as $key => $attribute)
+<div class="container-xxl flex-grow-1 container-p-y">
+	<h4 class="fw-bold py-3 mb-4">
+		<span class="text-muted fw-light"> </span>{{ translate('Attributes')}}
+	</h4>
+	<div class="row">
+		<div class="col-md-7">
+			<div class="card">
+				<div class="card-header">
+					<h5 class="mb-0 h6">Attributes</h5>
+				</div>
+				<div class="card-body">
+					<table class="table aiz-table mb-0 footable footable-1 breakpoint-lg">
+						<thead>
+							<tr class="footable-header">
+								<th class="footable-first-visible" style="display: table-cell;">#</th>
+								<th style="display: table-cell;">{{ translate('Name')}}</th>
+								<th style="display: table-cell;">{{ translate('Values')}}</th>
+								<th class="text-right footable-last-visible" style="display: table-cell;">{{ translate('Options')}}</th>
+							</tr>
+						</thead>
+						<tbody>
+							@foreach($attributes as $key => $attribute)
 							<tr>
-								<td>{{$key+1}}</td>
-								<td>{{$attribute->getTranslation('name')}}</td>
+								<td class="footable-first-visible" style="display: table-cell;">{{$key+1}}</td>
+								<td style="display: table-cell;">{{$attribute->getTranslation('name')}}</td>
 								<td>
 									@foreach($attribute->attribute_values as $key => $value)
-									<span class="badge badge-inline badge-md bg-soft-dark">{{ $value->value }}</span>
+									{{ $value->value }}
 									@endforeach
 								</td>
-								<td class="text-right">
+								<td>
 									<a class="btn btn-soft-info btn-icon btn-circle btn-sm" href="{{route('attributes.show', $attribute->id)}}" title="{{ translate('Attribute values') }}">
 										<i class="las la-cog"></i>
 									</a>
@@ -44,38 +57,48 @@
 									<a href="#" class="btn btn-soft-danger btn-icon btn-circle btn-sm confirm-delete" data-href="{{route('attributes.destroy', $attribute->id)}}" title="{{ translate('Delete') }}">
 										<i class="las la-trash"></i>
 									</a>
+									<a href="{{route('attributes.show', $attribute->id)}}" title="{{ translate('Attribute values') }}"> <i class="fa fa-cog" aria-hidden="true"></i></a>
+									<a href="{{route('attributes.edit', ['id'=>$attribute->id, 'lang'=>env('DEFAULT_LANGUAGE')] )}}" title="{{ translate('Edit') }}"><i class="bx bxs-edit"></i></a>
+									<a href="{{route('attributes.destroy', $attribute->id)}}" title="{{ translate('Delete') }}"> <i class="bx bx-trash me-1" aria-hidden="true"></i></a>
 								</td>
 							</tr>
-						@endforeach
-					</tbody>
-				</table>
+							@endforeach
+
+						</tbody>
+					</table>
+				</div>
 			</div>
 		</div>
-	</div>
-	<div class="col-md-5">
-		<div class="card">
-			<div class="card-header">
+		<div class="col-md-5">
+			<div class="card">
+				<div class="card-header">
 					<h5 class="mb-0 h6">{{ translate('Add New Attribute') }}</h5>
-			</div>
-			<div class="card-body">
-				<form action="{{ route('attributes.store') }}" method="POST">
-					@csrf
-					<div class="form-group mb-3">
-						<label for="name">{{translate('Name')}}</label>
-						<input type="text" placeholder="{{ translate('Name')}}" id="name" name="name" class="form-control" required>
-					</div>
-					<div class="form-group mb-3 text-right">
-						<button type="submit" class="btn btn-primary">{{translate('Save')}}</button>
-					</div>
-				</form>
+				</div>
+				<div class="card-body">
+					<form action="{{ route('attributes.store') }}" method="POST">
+						@csrf
+
+
+						<div class="form-group mb-3">
+							<label for="name">{{translate('Name')}}</label>
+							<input type="text" placeholder="{{ translate('Name')}}" id="name" name="name" class="form-control" required="">
+						</div>
+						<div class="form-group mb-3 text-right">
+							<button type="submit" class="btn btn-primary">{{translate('Save')}}</button>
+						</div>
+					</form>
+				</div>
 			</div>
 		</div>
-	</div>
 </div>
+
+
+
+
 
 
 @endsection
 
 @section('modal')
-    @include('modals.delete_modal')
+@include('modals.delete_modal')
 @endsection
