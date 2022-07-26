3ad3075de103599a1b07df6aa470a874e35c620a
AvinashSingh <avinash.orrish@gmail.com>
2022-07-26 18:14:34 +0530

    update css and js and dashboard

--- resources/views/backend/dashboard.blade.php
+++ resources/views/backend/dashboard.blade.php
@@ -10,82 +10,182 @@
     </div>
 @endif
 @if(Auth::user()->user_type == 'admin' || in_array('1', json_decode(Auth::user()->staff->role->permissions)))
+<!--1st Row Start -->
 <div class="row gutters-10">
-    <div class="col-lg-6">
+    <div class="col-lg-12">
         <div class="row gutters-10">
-            <div class="col-6">
-                <div class="bg-grad-2 text-white rounded-lg mb-4 overflow-hidden">
-                    <div class="px-3 pt-3">
+            <div class="col-3">
+                <div class="box-shadow-opacitys align-items-center justify-content-between text-white rounded-lg mb-4 overflow-hidden">
+                    <div class="px-3 pt-3 text-center">
                         <div class="opacity-50">
                             <span class="fs-12 d-block">{{ translate('Total') }}</span>
                             {{ translate('Customer') }}
                         </div>
+                        <i class="fa fa-solid fa-user"></i>
                         <div class="h3 fw-700 mb-3">
                             {{ \App\Models\User::where('user_type', 'customer')->where('email_verified_at', '!=', null)->count() }}
                         </div>
                     </div>
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-                        <path fill="rgba(255,255,255,0.3)" fill-opacity="1" d="M0,128L34.3,112C68.6,96,137,64,206,96C274.3,128,343,224,411,250.7C480,277,549,235,617,213.3C685.7,192,754,192,823,181.3C891.4,171,960,149,1029,117.3C1097.1,85,1166,43,1234,58.7C1302.9,75,1371,149,1406,186.7L1440,224L1440,320L1405.7,320C1371.4,320,1303,320,1234,320C1165.7,320,1097,320,1029,320C960,320,891,320,823,320C754.3,320,686,320,617,320C548.6,320,480,320,411,320C342.9,320,274,320,206,320C137.1,320,69,320,34,320L0,320Z"></path>
+                        <path fill="rgba(255,255,255,0.3)" fill-opacity="1"
+                            d="M0,128L34.3,112C68.6,96,137,64,206,96C274.3,128,343,224,411,250.7C480,277,549,235,617,213.3C685.7,192,754,192,823,181.3C891.4,171,960,149,1029,117.3C1097.1,85,1166,43,1234,58.7C1302.9,75,1371,149,1406,186.7L1440,224L1440,320L1405.7,320C1371.4,320,1303,320,1234,320C1165.7,320,1097,320,1029,320C960,320,891,320,823,320C754.3,320,686,320,617,320C548.6,320,480,320,411,320C342.9,320,274,320,206,320C137.1,320,69,320,34,320L0,320Z">
+                        </path>
                     </svg>
                 </div>
             </div>
-            <div class="col-6">
-                <div class="bg-grad-3 text-white rounded-lg mb-4 overflow-hidden">
-                    <div class="px-3 pt-3">
+            <div class="col-3">
+                <div class="box-shadow-opacitys align-items-center justify-content-between text-white rounded-lg mb-4 overflow-hidden">
+                    <div class="px-3 pt-3 text-center">
                         <div class="opacity-50">
                             <span class="fs-12 d-block">{{ translate('Total') }}</span>
                             {{ translate('Order') }}
                         </div>
+                        <i class="fa fa-solid fa-box"></i>
                         <div class="h3 fw-700 mb-3">{{ \App\Models\Order::count() }}</div>
                     </div>
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-                        <path fill="rgba(255,255,255,0.3)" fill-opacity="1" d="M0,128L34.3,112C68.6,96,137,64,206,96C274.3,128,343,224,411,250.7C480,277,549,235,617,213.3C685.7,192,754,192,823,181.3C891.4,171,960,149,1029,117.3C1097.1,85,1166,43,1234,58.7C1302.9,75,1371,149,1406,186.7L1440,224L1440,320L1405.7,320C1371.4,320,1303,320,1234,320C1165.7,320,1097,320,1029,320C960,320,891,320,823,320C754.3,320,686,320,617,320C548.6,320,480,320,411,320C342.9,320,274,320,206,320C137.1,320,69,320,34,320L0,320Z"></path>
+                        <path fill="rgba(255,255,255,0.3)" fill-opacity="1"
+                            d="M0,128L34.3,112C68.6,96,137,64,206,96C274.3,128,343,224,411,250.7C480,277,549,235,617,213.3C685.7,192,754,192,823,181.3C891.4,171,960,149,1029,117.3C1097.1,85,1166,43,1234,58.7C1302.9,75,1371,149,1406,186.7L1440,224L1440,320L1405.7,320C1371.4,320,1303,320,1234,320C1165.7,320,1097,320,1029,320C960,320,891,320,823,320C754.3,320,686,320,617,320C548.6,320,480,320,411,320C342.9,320,274,320,206,320C137.1,320,69,320,34,320L0,320Z">
+                        </path>
                     </svg>
                 </div>
             </div>
-            <div class="col-6">
-                <div class="bg-grad-1 text-white rounded-lg mb-4 overflow-hidden">
-                    <div class="px-3 pt-3">
+            <div class="col-3">
+                <div class="box-shadow-opacitys align-items-center justify-content-between  text-white rounded-lg mb-4 overflow-hidden">
+                    <div class="px-3 pt-3 text-center">
                         <div class="opacity-50">
                             <span class="fs-12 d-block">{{ translate('Total') }}</span>
                             {{ translate('Product category') }}
                         </div>
+                        <i class="fa fa-solid fa-cube"></i>
                         <div class="h3 fw-700 mb-3">{{ \App\Models\Category::count() }}</div>
                     </div>
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-                        <path fill="rgba(255,255,255,0.3)" fill-opacity="1" d="M0,128L34.3,112C68.6,96,137,64,206,96C274.3,128,343,224,411,250.7C480,277,549,235,617,213.3C685.7,192,754,192,823,181.3C891.4,171,960,149,1029,117.3C1097.1,85,1166,43,1234,58.7C1302.9,75,1371,149,1406,186.7L1440,224L1440,320L1405.7,320C1371.4,320,1303,320,1234,320C1165.7,320,1097,320,1029,320C960,320,891,320,823,320C754.3,320,686,320,617,320C548.6,320,480,320,411,320C342.9,320,274,320,206,320C137.1,320,69,320,34,320L0,320Z"></path>
+                        <path fill="rgba(255,255,255,0.3)" fill-opacity="1"
+                            d="M0,128L34.3,112C68.6,96,137,64,206,96C274.3,128,343,224,411,250.7C480,277,549,235,617,213.3C685.7,192,754,192,823,181.3C891.4,171,960,149,1029,117.3C1097.1,85,1166,43,1234,58.7C1302.9,75,1371,149,1406,186.7L1440,224L1440,320L1405.7,320C1371.4,320,1303,320,1234,320C1165.7,320,1097,320,1029,320C960,320,891,320,823,320C754.3,320,686,320,617,320C548.6,320,480,320,411,320C342.9,320,274,320,206,320C137.1,320,69,320,34,320L0,320Z">
+                        </path>
                     </svg>
                 </div>
             </div>
-            <div class="col-6">
-                <div class="bg-grad-4 text-white rounded-lg mb-4 overflow-hidden">
-                    <div class="px-3 pt-3">
+            <div class="col-3">
+                <div class=" box-shadow-opacitys text-white rounded-lg mb-4 overflow-hidden">
+                    <div class="px-3 pt-3 text-center">
                         <div class="opacity-50">
                             <span class="fs-12 d-block">{{ translate('Total') }}</span>
                             {{ translate('Product brand') }}
                         </div>
+                        <i class="fa fa-solid fa-desktop"></i>
                         <div class="h3 fw-700 mb-3">{{ \App\Models\Brand::count() }}</div>
                     </div>
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-                        <path fill="rgba(255,255,255,0.3)" fill-opacity="1" d="M0,128L34.3,112C68.6,96,137,64,206,96C274.3,128,343,224,411,250.7C480,277,549,235,617,213.3C685.7,192,754,192,823,181.3C891.4,171,960,149,1029,117.3C1097.1,85,1166,43,1234,58.7C1302.9,75,1371,149,1406,186.7L1440,224L1440,320L1405.7,320C1371.4,320,1303,320,1234,320C1165.7,320,1097,320,1029,320C960,320,891,320,823,320C754.3,320,686,320,617,320C548.6,320,480,320,411,320C342.9,320,274,320,206,320C137.1,320,69,320,34,320L0,320Z"></path>
+                        <path fill="rgba(255,255,255,0.3)" fill-opacity="1"
+                            d="M0,128L34.3,112C68.6,96,137,64,206,96C274.3,128,343,224,411,250.7C480,277,549,235,617,213.3C685.7,192,754,192,823,181.3C891.4,171,960,149,1029,117.3C1097.1,85,1166,43,1234,58.7C1302.9,75,1371,149,1406,186.7L1440,224L1440,320L1405.7,320C1371.4,320,1303,320,1234,320C1165.7,320,1097,320,1029,320C960,320,891,320,823,320C754.3,320,686,320,617,320C548.6,320,480,320,411,320C342.9,320,274,320,206,320C137.1,320,69,320,34,320L0,320Z">
+                        </path>
                     </svg>
                 </div>
             </div>
         </div>
     </div>
+</div>
+<!--1st Row End -->
 
-    <div class="col-lg-6">
+
+<div class="row gutters-10">
+    <div class="col-lg-12">
         <div class="row gutters-10">
+            <!-- Product graph Start -->
             <div class="col-6">
                 <div class="card">
                     <div class="card-header">
                         <h6 class="mb-0 fs-14">{{ translate('Products') }}</h6>
                     </div>
                     <div class="card-body">
-                        <canvas id="pie-1" class="w-100" height="305"></canvas>
+                        
+                        <div class="d-flex justify-content-between align-items-center mb-4" style="position: relative;">
+                            <!-- <div class="d-flex flex-column align-items-center gap-1">
+                                <h2 class="mb-2">8,258</h2>
+                                <span>Total Orders</span>
+                            </div> -->
+
+                            <div id="orderStatisticsChart" style="min-height: 137.55px;">
+                                <canvas id="pie-1" class="w-100" height="305"></canvas>
+                            </div>
+                        </div>
+
+                        <ul class="p-0 m-0">
+                            <li class="d-flex mb-4 pb-1">
+                                <div class="avatar flex-shrink-0 me-3">
+                                    <span class="avatar-initial rounded bg-label-primary"><i
+                                            class="fa fa-mobile-alt"></i></span>
+                                </div>
+                                <div class="d-flex w-100 flex-wrap align-items-center justify-content-between gap-2">
+                                    <div class="me-2">
+                                        <h6 class="mb-0">Total Published </h6>
+                                        <small class="text-muted">Products</small>
+                                    </div>
+                                    <div class="user-progress">
+                                        <small class="fw-semibold">
+                                            {{ \App\Models\Product::where('published', 1)->count() }}
+                                        </small>
+                                    </div>
+                                </div>
+                            </li>
+                            <li class="d-flex mb-4 pb-1">
+                                <div class="avatar flex-shrink-0 me-3">
+                                    <span class="avatar-initial rounded bg-label-success"><i class="fa fa-cubes"></i></span>
+                                </div>
+                                <div class="d-flex w-100 flex-wrap align-items-center justify-content-between gap-2">
+                                    <div class="me-2">
+                                        <h6 class="mb-0">Total Un-published</h6>
+                                        <small class="text-muted">Products</small>
+                                    </div>
+                                    <div class="user-progress">
+                                        <small class="fw-semibold">
+                                            {{ \App\Models\Product::where('published', 0)->count() }}
+                                        </small>
+                                    </div>
+                                </div>
+                            </li>
+                            <li class="d-flex mb-4 pb-1">
+                                <div class="avatar flex-shrink-0 me-3">
+                                    <span class="avatar-initial rounded bg-label-info"><i class="fa fa-home"></i></span>
+                                </div>
+                                <div class="d-flex w-100 flex-wrap align-items-center justify-content-between gap-2">
+                                    <div class="me-2">
+                                        <h6 class="mb-0">Total Vender</h6>
+                                        <small class="text-muted">Products</small>
+                                    </div>
+                                    <div class="user-progress">
+                                        <small class="fw-semibold">
+                                            {{ \App\Models\Product::where('published', 1)->where('added_by', 'seller')->count() }}
+                                        </small>
+                                    </div>
+                                </div>
+                            </li>
+                            <li class="d-flex">
+                                <div class="avatar flex-shrink-0 me-3">
+                                    <span class="avatar-initial rounded bg-label-secondary">
+                                        <i class="fa fa-solid fa-futbol"></i>
+                                    </span>
+                                </div>
+                                <div class="d-flex w-100 flex-wrap align-items-center justify-content-between gap-2">
+                                    <div class="me-2">
+                                        <h6 class="mb-0">Total Admin</h6>
+                                        <small class="text-muted">Products</small>
+                                    </div>
+                                    <div class="user-progress">
+                                        <small class="fw-semibold">
+                                        {{ \App\Models\Product::where('published', 1)->where('added_by', 'admin')->count() }}
+                                        </small>
+                                    </div>
+                                </div>
+                            </li>
+                        </ul>
                     </div>
                 </div>
             </div>
+
+            <!-- Seller graph Start -->
             <div class="col-6">
                 <div class="card">
                     <div class="card-header">
@@ -96,6 +196,7 @@
                     </div>
                 </div>
             </div>
+
         </div>
     </div>
 </div>
@@ -177,6 +278,7 @@
         data: {
             labels: [
                 '{{translate('Total published products')}}',
+                '{{translate('Total unpublished products')}}',
                 '{{translate('Total sellers products')}}',
                 '{{translate('Total admin products')}}'
             ],
@@ -184,14 +286,15 @@
                 {
                     data: [
                         {{ \App\Models\Product::where('published', 1)->count() }},
+                        {{ \App\Models\Product::where('published', 0)->count() }},
                         {{ \App\Models\Product::where('published', 1)->where('added_by', 'seller')->count() }},
                         {{ \App\Models\Product::where('published', 1)->where('added_by', 'admin')->count() }}
                     ],
                     backgroundColor: [
-                        "#fd3995",
-                        "#34bfa3",
-                        "#5d78ff",
-                        '#fdcb6e',
+                        "#696cff",
+                        "#8592a3",
+                        "#03c3ec",
+                        '#71dd37',
                         '#d35400',
                         '#8e44ad',
                         '#006442',
@@ -203,7 +306,7 @@
             ]
         },
         options: {
-            cutoutPercentage: 70,
+            cutoutPercentage: 80,
             legend: {
                 labels: {
                     fontFamily: 'Poppins',
